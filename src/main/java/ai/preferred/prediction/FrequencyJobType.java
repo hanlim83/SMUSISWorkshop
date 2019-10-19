fbc15109f6d3501aa385cacf03c824b4ff561419
winstonlimjy <winstonlim.2000@gmail.com>
2019-10-19 21:22:29 +0800

    Linear Regression for Salary against Job type

--- src/main/java/ai/preferred/prediction/FrequencyJobType.java
+++ src/main/java/ai/preferred/prediction/FrequencyJobType.java
@@ -1,6 +1,9 @@
 package ai.preferred.prediction;
 
+import ai.preferred.regression.PlotLinearRegression;
+import ai.preferred.regression.PrintRegression;
 import ai.preferred.regression.Shell;
+import ai.preferred.regression.TrainLinearRegression;
 import ai.preferred.regression.pe.EncodeTextAsFrequency;
 import ai.preferred.regression.pe.ProjectColumns;
 import ai.preferred.regression.pe.RemoveColumn;
@@ -13,35 +16,53 @@ public class FrequencyJobType {
         Shell.help(EncodeTextAsFrequencyNoWhitespace.class);
 
         //Moving salary to first col
-        Shell.run(SwapColumns.class,"-i data_salary.csv -o updatedDataV1.csv -x 0 -y 3");
+        Shell.run(SwapColumns.class,"-i data_salary.csv -o updatedDataV1.csv -x 0 -y 8");
         //Moving Type to 2nd col
-        Shell.run(SwapColumns.class,"-i updatedDataV1.csv -o updatedDataV2.csv -x 1 -y 5");
+        Shell.run(SwapColumns.class,"-i updatedDataV1.csv -o updatedDataV2.csv -x 1 -y 4");
         //Removing remaining columns
         Shell.run(RemoveColumn.class,"-i updatedDataV2.csv -o updatedDataV3.csv -c 2");
         Shell.run(RemoveColumn.class,"-i updatedDataV3.csv -o updatedDataV4.csv -c 2");
         Shell.run(RemoveColumn.class,"-i updatedDataV4.csv -o updatedDataV5.csv -c 2");
         Shell.run(RemoveColumn.class,"-i updatedDataV5.csv -o updatedDataV6.csv -c 2");
         Shell.run(RemoveColumn.class,"-i updatedDataV6.csv -o updatedDataV7.csv -c 2");
+        Shell.run(RemoveColumn.class,"-i updatedDataV7.csv -o updatedDataV8.csv -c 2");
+        Shell.run(RemoveColumn.class,"-i updatedDataV8.csv -o updatedDataV9.csv -c 2");
+
 
         //Splitting | as well as " ";
-        Shell.run(EncodeTextAsFrequencyNoWhitespace.class,"-i updatedDataV7.csv -o updatedDataV8.csv -c 1");
+        Shell.run(EncodeTextAsFrequencyNoWhitespace.class,"-i updatedDataV9.csv -o updatedDataV10.csv -c 1");
 
-        //Salary -> Co-Founder
-        Shell.run(ProjectColumns.class,"-i updatedDataV8.csv -o salary_cofounder.csv -c salary WORD:co-founder ");
+        //Salary against various job type
+        Shell.run(ProjectColumns.class,"-i updatedDataV10.csv -o salary_contract.csv -c meanSalary WORD:contract ");
+        Shell.run(ProjectColumns.class,"-i updatedDataV10.csv -o salary_freelance.csv -c meanSalary WORD:freelance ");
+        Shell.run(ProjectColumns.class,"-i updatedDataV10.csv -o salary_fulltime.csv -c meanSalary WORD:full-time ");
+        Shell.run(ProjectColumns.class,"-i updatedDataV10.csv -o salary_internship.csv -c meanSalary WORD:internship ");
+        Shell.run(ProjectColumns.class,"-i updatedDataV10.csv -o salary_parttime.csv -c meanSalary WORD:part-time ");
+        Shell.run(ProjectColumns.class,"-i updatedDataV10.csv -o salary_temporary.csv -c meanSalary WORD:temporary ");
 
-        Shell.run(ProjectColumns.class,"-i updatedDataV8.csv -o salary_contract.csv -c salary WORD:contract ");
+        //Training model for individual job type
+        Shell.run(TrainLinearRegression.class, "-i salary_contract.csv -m temp/salary_contract_model.model");
+        Shell.run(PrintRegression.class,"-i salary_contract.csv -m temp/salary_contract_model.model");
+        Shell.run(PlotLinearRegression.class,"-i salary_contract.csv -m temp/salary_contract_model.model");
 
-        Shell.run(ProjectColumns.class,"-i updatedDataV8.csv -o salary_freelance.csv -c salary WORD:freelance ");
+        Shell.run(TrainLinearRegression.class, "-i salary_freelance.csv -m temp/salary_freelance_model.model");
+        Shell.run(PrintRegression.class,"-i salary_freelance.csv -m temp/salary_freelance_model.model");
+        Shell.run(PlotLinearRegression.class,"-i salary_freelance.csv -m temp/salary_freelance_model.model");
 
-        Shell.run(ProjectColumns.class,"-i updatedDataV8.csv -o salary_fulltime.csv -c salary WORD:full-time ");
+        Shell.run(TrainLinearRegression.class, "-i salary_fulltime.csv -m temp/salary_fulltime_model.model");
+        Shell.run(PrintRegression.class,"-i salary_fulltime.csv -m temp/salary_fulltime_model.model");
+        Shell.run(PlotLinearRegression.class,"-i salary_fulltime.csv -m temp/salary_fulltime_model.model");
 
-        Shell.run(ProjectColumns.class,"-i updatedDataV8.csv -o salary_internship.csv -c salary WORD:internship ");
+        Shell.run(TrainLinearRegression.class, "-i salary_internship.csv -m temp/salary_internship_model.model");
+        Shell.run(PrintRegression.class,"-i salary_internship.csv -m temp/salary_internship_model.model");
+        Shell.run(PlotLinearRegression.class,"-i salary_internship.csv -m temp/salary_internship_model.model");
 
-        Shell.run(ProjectColumns.class,"-i updatedDataV8.csv -o salary_parttime.csv -c salary WORD:part-time ");
+        Shell.run(TrainLinearRegression.class, "-i salary_parttime.csv -m temp/salary_parttime_model.model");
+        Shell.run(PrintRegression.class,"-i salary_parttime.csv -m temp/salary_parttime_model.model");
+        Shell.run(PlotLinearRegression.class,"-i salary_parttime.csv -m temp/salary_parttime_model.model");
 
-        Shell.run(ProjectColumns.class,"-i updatedDataV8.csv -o salary_temporary.csv -c salary WORD:temporary ");
-
-//        Shell.run(SwapColumns.class,"-i data.csv -o salaryFirstCol.csv ")
-//        Shell.run(ProjectColumns.class,"-i data.csv -o JobType.csv ")
+        Shell.run(TrainLinearRegression.class, "-i salary_temporary.csv -m temp/salary_temporary_model.model");
+        Shell.run(PrintRegression.class,"-i salary_temporary.csv -m temp/salary_temporary_model.model");
+        Shell.run(PlotLinearRegression.class,"-i salary_temporary.csv -m temp/salary_temporary_model.model");
     }
 }
